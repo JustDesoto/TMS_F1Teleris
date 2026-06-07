d4638728741ad17298b955ef3a304bc11dc42a4a
JustDesoto <135348475+JustDesoto@users.noreply.github.com>
2026-06-07 16:22:57 +0300

    Add files via upload

---
+++ raw/openf1_laps.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field, field_validator
+from datetime import datetime
+from typing import Optional
+
+class OpenF1Lap(BaseModel):
+    """Provides detailed information about individual laps"""
+    
+    date_start: datetime # Время начала круга
+    driver_number: int
+    duration_sector_1: Optional[float] = None
+    duration_sector_2: Optional[float] = None
+    duration_sector_3: Optional[float] = None
+    i1_speed: Optional[int] = None
+    i2_speed: Optional[int] = None
+    is_pit_out_lap: bool
+    lap_duration: float
+    lap_number: int
+    meeting_key: int
+    segments_sector_1: Optional[list[int]] = None
+    segments_sector_2: Optional[list[int]] = None
+    segments_sector_3: Optional[list[int]] = None
+    session_key: int
+    st_speed:Optional[int] = None # Скорость на speed trap, где обычно замеряют максимальную скорость
+    
+    etl_loaded_at: datetime = Field(default_factory=datetime.utcnow)
+    etl_source: str = "openf1"
+    etl_endpoint: str = "laps"
